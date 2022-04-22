9e2fd5a9aea7628fa7e31363d1de5ea5b4f740c7
ndourbamba18 <ndourbamba18@gmail.com>
2022-04-22 16:15:24 +0100

    First Commit

---
+++ src/main/java/com/ndourcodeur/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.ndourcodeur.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    /**
+     *  URL ====> http://localhost:8080/api/v1/demo
+     */
+    @GetMapping("/")
+    public ResponseEntity<?> findAll(){
+        return new ResponseEntity<>("Hello and Welcome to NdourCodeur :)", HttpStatus.OK);
+    }
+}
